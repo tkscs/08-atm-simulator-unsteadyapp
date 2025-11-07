5e837abf91e4b8ba01abc961b32e5651d0e5a400
unsteadyapp <akanodia2029@kehillahstudent.org>
2025-11-07 11:36:59 -0800

    finished

--- main.py
+++ main.py
@@ -17,3 +17,36 @@ Options:
 # start with 1 million dollars
 balance = 1000000
 
+while True:
+    UInput = input("Enter Action: ").lower().replace(" ","")
+    if(UInput == "checkbalence"):
+        print(f"Your balence is :{balance}")
+    elif(UInput == "widthdraw"):
+        try:
+            howmuch = int(input("how much to widthdraw? "))
+        except TypeError:
+            print("must be an integer!")
+        else:
+            if(balance>=howmuch and howmuch>=0):
+                balance = balance - howmuch
+                print(f"You widthdrew {howmuch} & have {balance} left.")
+            else:
+                print("Negitive or greater than balance")
+    elif(UInput=="deposit"):
+        try:
+            howmuch = int(input("how much to deposit? "))
+        except TypeError:
+            print("must be an integer!")
+        else:
+            if(howmuch>=0):
+                balance = balance + howmuch
+                print(f"You deposited {howmuch} & have {balance} left.")
+            else:
+                print("howmuch must be postive!")
+    else:
+        print(f"unrecognized input: {UInput}. Try checkbalence, deposit, or widthdraw")
+        
+        
+        
+
+
